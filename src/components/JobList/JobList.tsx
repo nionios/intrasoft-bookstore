0dea45cba948372046e94872ac276e253addfdc3
nionios <dennis.nik@protonmail.com>
2024-02-04 14:40:22 +0200

    Job list now stops when api responds with empty array

--- src/components/JobList/JobList.tsx
+++ src/components/JobList/JobList.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Spinner from "@/components/Spinner/Spinner";
 import InfiniteScroll from 'react-infinite-scroll-component';
 import axios from 'axios';
@@ -11,7 +12,6 @@ import JobBox from "@/components/JobBox/JobBox";
  */
 const populateJobBoxes = (jobs: Array) => {
     const jobBoxes: Array<typeof JobBox> = [];
-    console.log(jobs)
     jobs.forEach(job => {
         jobBoxes.push(JobBox(job));
     });
@@ -23,9 +23,9 @@ const populateJobBoxes = (jobs: Array) => {
  * Retrieve job posts from server to display them in UI
  * @param cookies {Cookies} The cookies provided by next-client-cookies
  * @param page {number} The page number the user is currently scrolled to.
- * @returns {JobBox[] | []} Retrieved jobs in JobBox component array form from populateJobBoxes().
+ * @returns {JobBox[] | null} Retrieved jobs in JobBox component array form from populateJobBoxes().
  */
-const fetchJobs = (cookies: Cookies, page: number) => {
+const fetchJobs = async (cookies: Cookies, page: number) => {
     const config = {
         url: `/api/jobs/all`,
         method: 'post',
@@ -39,26 +39,25 @@ const fetchJobs = (cookies: Cookies, page: number) => {
         },
     }
     // Make axios request to api and get the job posts.
-    return axios(config)
-        .then((apiResponse) => {
-            if (apiResponse.status === 200) {
-                // If apiResponse is empty array, then return [] and don't try to populate jobBoxes.
-                if (apiResponse.data.items.length !== 0) {
-                    return populateJobBoxes(apiResponse.data.items);
-                } else {
-                    return [];
-                }
+    try {
+        let apiResponse = await axios(config);
+        if (apiResponse.status === 200) {
+            // If apiResponse is empty array, then return [] and don't try to populate jobBoxes.
+            if (apiResponse.data.items.length !== 0) {
+                return populateJobBoxes(apiResponse.data.items);
             } else {
-                // If response status is 400, then pop up login modal, means auth was not successful.
-                // This either means auth token has expired or not valid.
-                document.location.href = "/login";
-                return [];
+                return null;
             }
-        })
-        .catch((error) => {
-            console.error(error);
-            return new Array(JobBox(0));
-        });
+        } else {
+            // If response status is 400, then pop up login modal, means auth was not successful.
+            // This either means auth token has expired or not valid.
+            document.location.href = "/login";
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
 /**
@@ -73,22 +72,25 @@ export default function JobList(props: { initialJobs: Array<any> }) {
     const [jobBoxes, setJobPosts_] = useState(populateJobBoxes(props.initialJobs));
     // hasMore is needed to stop querying the server for job posts once all the available jobs are displayed.
     const [hasMore, setHasMore] = useState(true);
-
     /**
      * We start with page 2, since the first page is already pre-fetched when we access homepage. @see preFetchJobs()
      */
     const [jobPage, setJobPage] = useState(2);
 
+
     /**
      * Wrapper around setJobPosts_ that calls fetchJobs and pushes results to already fetched jobs.
      */
-    const updateJobPosts = () => {
-        let previousJobNumber : number = jobBoxes.length;
-        setJobPosts_(jobBoxes => [...jobBoxes, fetchJobs(cookies, jobPage)]);
-        // If no more jobPosts are fetched from server, stop running fetch jobs.
-        if (previousJobNumber === jobBoxes.length) setHasMore(false);
-        // Increment the job page by 1 every time jobs are fetched.
-        else setJobPage(jobPage+1);
+    const updateJobPosts = async () => {
+        // If fetched jobs are null, update state as to not try to fetch anymore jobs and dont update jobBoxes.
+        const fetchedJobPosts = await fetchJobs(cookies, jobPage);
+        if (!fetchedJobPosts) {
+            setHasMore(false);
+        } else {
+            setJobPosts_(jobBoxes => [...jobBoxes, fetchedJobPosts]);
+            // Increment the job page by 1 every time jobs are fetched.
+            setJobPage(jobPage + 1);
+        }
     }
 
     return (
